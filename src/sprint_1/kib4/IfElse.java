a77cde6ab2bf25e17aff4c37d3d13f5e11df397e
Andrea Kibler <andreamkibler@gmail.com>
2017-11-01 18:13:59 -0500

    if else if else updates

---
+++ src/sprint_1/kib4/IfElse.java
@@ -0,0 +1,20 @@
+package sprint_1.kib4;
+
+// Write a single class with a single main method that demonstrates the use of an if -> else/if -> else ladder.
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        if (i < 5) {
+            System.out.println("something");
+
+        } else if (i < 10) {
+            System.out.println("something else");
+
+        } else {
+            System.out.println("nothing");
+        }
+    }
+}

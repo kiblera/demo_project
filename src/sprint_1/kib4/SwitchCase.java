db45e29cd7cc2a96d20b5c7d99b9c3426556a0ed
Andrea Kibler <andreamkibler@gmail.com>
2017-11-01 22:17:14 -0500

    switch case updates

---
+++ src/sprint_1/kib4/SwitchCase.java
@@ -0,0 +1,45 @@
+package sprint_1.kib4;
+
+
+// Create a class with a single main method that demonstrates the full use of a switch case.
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter a whole number between 1 and 10.");
+        Scanner input = new Scanner(System.in);
+        int day = input.nextInt();
+        String dayString;
+
+        switch (day) {
+            case 1:
+                dayString = "Sunday";
+                break;
+            case 2:
+                dayString = "Monday";
+                break;
+            case 3:
+                dayString = "Tuesday";
+                break;
+            case 4:
+                dayString = "Wednesday";
+                break;
+            case 5:
+                dayString = "Thursday";
+                break;
+            case 6:
+                dayString = "Friday";
+                break;
+            case 7:
+                dayString = "Saturday";
+                break;
+            default:
+                dayString = "invalid day";
+                break;
+        }
+        System.out.println(dayString);
+    }
+}
---
+++ src/sprint_1/kib4/WhileLoops.java
@@ -0,0 +1,10 @@
+package sprint_1.kib4;
+
+// Create a class with a single main method that demonstrates both the while and the do-while loops in action.
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+    }
+}

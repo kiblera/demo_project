b38de4c77a2a03387c0dbe802e9da6f3eaf20bf5
Andrea Kibler <andreamkibler@gmail.com>
2017-11-01 22:55:44 -0500

    while loop updates

--- src/sprint_1/kib4/WhileLoops.java
+++ src/sprint_1/kib4/WhileLoops.java
@@ -5,6 +5,24 @@ package sprint_1.kib4;
 public class WhileLoops {
 
     public static void main(String[] args) {
+        int count = 1;
+        int x = 0;
 
+        int i = doWhile (x);
+
+        while (count < 10) {
+            System.out.println("Count + " + count);
+            count++;
+        }
+    }
+
+    public static int doWhile(int i) {
+        int x = 0;
+
+        do {
+            System.out.println("Number is " + x);
+            x++;
+        } while (x < 5);
+        return x;
     }
 }
